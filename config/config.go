e61ec1e412b6d2c50de7443ae36aa906c330d556
Andtherya <Andy17998@outlook.com>
2025-10-07 09:58:09 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -21,6 +21,7 @@ type Config struct {
 	AccessToken    string `json:"access_token"`     // Authentication token for API access
 	IPv4           string `json:"ipv4"`             // Assigned IPv4 address
 	IPv6           string `json:"ipv6"`             // Assigned IPv6 address
+	Socks5Proxy    string `json:"socks5_proxy" yaml:"socks5_proxy"`
 }
 
 // AppConfig holds the global application configuration.
